9551fd82dc645989bdc7bbb26e455fea60440633
Barna Mink <barnamink@gmail.com>
2019-06-26 16:16:16 -0700

    .

--- samptbl.c
+++ samptbl.c
@@ -1,6 +1,59 @@
+#include <errno.h>
 #include "samptbl.h"
 
-static sample_t	samptbl[SAMPTBL_SIZ];
+static sample_t	samples[SAMPTBL_SIZ];
+
+static int samptbl_inited = 0;
 
 
+int
+samptbl_init(int mode)
+{
+	int	i;
 
+	memset(samples, 0, sizeof(sample_t) * SAMPTBL_SIZ);
+
+	for(i = 0; i < SAMPTBL_SIZ; ++i) {
+
+		if(mode == SAMPTBL_MODE_RELOAD)
+			samples[i].sa_action = SAMPACT_DEL;
+		else
+			samples[i].sa_action = SAMPACT_NONE;
+
+	}
+
+	++samptbl_inited;
+
+	return 0;
+}
+
+
+void
+samptbl_uninit(void)
+{
+	int	i;
+
+	if(!samptbl_inited)
+		return;
+
+	for(i = 0; i < SAMPTBL_SIZ; ++i) {
+		buninit(&samples[i].sa_data);
+	}
+
+	memset(samples, 0, sizeof(sample_t) * SAMPTBL_SIZ);
+}
+
+
+int
+samptbl_del(int idx)
+{
+	if(idx < 0 || idx >= SAMPTBL_SIZ)
+		return EINVAL;
+
+	if(!samptbl_inited)
+		return ENOEXEC;
+
+	samples[idx].sa_action = SAMPACT_DEL;
+
+	return 0;
+}
--- samptbl.h
+++ samptbl.h
@@ -8,19 +8,23 @@
 #define SAMPACT_DEL	2	
 
 typedef struct sample {
-	int		*sa_action;
-	int		*sa_idx;
+	int		sa_action;
+	int		sa_idx;
 	bstr_t		*sa_data;
 } sample_t;
 
 #define SAMPTBL_SIZ	100
 
+#define SAMPTBL_MODE_INCR	0
+#define SAMPTBL_MODE_RELOAD	1
 
 int samptbl_init(int);
+void samptbl_uninit(void);
+
 int samptbl_add(int, const char *);
 int samptbl_del(int);
-int samptbl_uninit();
 
+int samptbl_convert(bstr_t **);
 
 
 #endif
